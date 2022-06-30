260df7e8ea4346c566f3ae7865fb7740610d07bc
Camila Fuentes Maldonado <107515087+KmiFM@users.noreply.github.com>
2022-06-30 18:39:23 -0300

    Add files via upload

---
+++ conceptos js/js/holamundo.js
@@ -0,0 +1,23 @@
+// para un comentario de una única linea
+/*Esto es un comentario de 
+multiples 
+líneas*/
+
+//mostrar un mensaje en la pantalla o documento
+document.write('<h1>hola kmi </h1>')
+//mostrar un mensaje en la consola
+console.log('prueba de consola :D')
+//mostrar ventana emergente o alert
+alert('desde una ventana emergente');
+//sumar dos valores
+document.write(12+5)
+
+document.write('<br> esto es una prueba')
+//para crear una variable selecciono entre var, let y const
+let nombre = prompt('Ingrese su nombre')
+
+console.log(nombre);
+document.write('<p>Hola' + nombre + '</p>'); 
+//ejemplo de variable constante
+const url = 'https://campus.rollingcodeschool.com/'
+document.write(url);
