e2b0b378a523be137ac77b17119b80980b0c4a60
Cami <camilafuentesmaldonado@gmail.com>
2022-07-07 17:19:31 -0300

    agregamos ejemplo de do while

---
+++ js/bucles.js
@@ -0,0 +1,33 @@
+/* Bucles
+
+while(condicion logica){
+    todas las lineas de codigo que quieroq ue repita el programa 
+    cambiar la condicion logica
+}
+
+*/
+
+
+// let cantidad = 1;
+// let opcion = 1;
+
+// while(cantidad <= 20){
+// document.write("<br> preparar mate y entregarlo al siguiente compañero, mate cebado n: "+ cantidad);
+ 
+// console.log((opcion += opcion));
+// cantidad++ ; // cantidad= cantidad+1; // cantidad += cantidad  
+// }
+
+
+/* Bucle Do-while hacer...mientras
+
+do{
+ todo el codigo que quiero ejecutar x cantidad de veces
+ cambiar la condicion logica
+}while(condicion logica) 
+*/
+let mate = 1;
+do{
+    document.write( "<br> preparar mate y entregarlo al siguiente compañero, mate cebado n:" + mate);
+    mate++;
+}while(mate <= 20)
