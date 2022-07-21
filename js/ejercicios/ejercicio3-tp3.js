84d1ee8cf544526cfd59ff398a52bdba6c3a244d
Cami <camilafuentesmaldonado@gmail.com>
2022-07-21 17:05:03 -0300

    agregué el css porque se me perdio no se por qué

--- js/ejercicios/ejercicio3-tp3.js
+++ js/ejercicios/ejercicio3-tp3.js
@@ -10,7 +10,7 @@ let sumas = [];
 let pruebaDado1 = [];
 let pruebaDado2 = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 50; i++) {
   let dado1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   let dado2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   let suma = dado1 + dado2;
@@ -24,23 +24,20 @@ document.write('<br>Resultados del segundo dado: '+pruebaDado2);
 document.write('<br>Resultados de la suma: '+sumas);
 
 
-
-document.write(`<table>
+document.write(`<table> 
 <tbody>
-<tr>
-<td> Resultado </td>
-<td> Frecuencias </td>
-</tr>
-`);
-let contador=0;
-for(let resultado =2; resultado <= 12; resultado++){
-    for(let posicion =0;posicion <sumas.length; posicion++){
-        if(resultado ===sumas [posicion]){
-            //sumar las aparciiones
+<tr><td>Resultado</td><td>Frecuencia</td></tr>
+`)
+
+for( let resultado =2; resultado <=12; resultado++){
+    let contador=0;
+    for(let posicion =0; posicion < sumas.length; posicion++){
+        if(resultado === sumas[posicion]){
+            //sumar las apariciones
+            contador++
         }
     }
-    document.write(`<tr> <td> ${resultado} </td> <td> ${contador} </td> </tr>`);
+    document.write(`<tr><td>${resultado}</td><td>${contador}</td></tr>`);
 }
-document.write(`
-</tbody>
-</table>`);
+
+document.write(`</tbody></table>`)
